002cda7192c682609ae23454be6c0ac959b3d308
Vandana Sivadasan <sivadasanvandana62@gmail.com>
2020-04-28 09:48:30 -0500

    Updates

---
+++ precache-manifest.8288ebc864f137c5332b78ef709d4859.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d87a2524964bf1f33cc3efe7b63202e",
+    "url": "/Unit-19-React-ED/index.html"
+  },
+  {
+    "revision": "c09dcb800919cff2afc7",
+    "url": "/Unit-19-React-ED/static/css/main.f0ccc4c2.chunk.css"
+  },
+  {
+    "revision": "1c094269bae9ff3ea815",
+    "url": "/Unit-19-React-ED/static/js/2.033f3caa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Unit-19-React-ED/static/js/2.033f3caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c09dcb800919cff2afc7",
+    "url": "/Unit-19-React-ED/static/js/main.8b593925.chunk.js"
+  },
+  {
+    "revision": "8c5bed236596156813eb",
+    "url": "/Unit-19-React-ED/static/js/runtime-main.0aca98c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Unit-19-React-ED/static/media/logo.5d5d9eef.svg"
+  }
+]);
